0b96c68471edf1a2e265e1fbe5fe2bdad865940a
Wesleyyy09 <231713634+Wesleyyy09@users.noreply.github.com>
2025-10-14 14:07:09 +0200

    ajout du gamehandler

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"net/http"
+	"strconv"
 )
 
 // Plateau 6x7 (0=vide, 1=rouge, 2=jaune)
@@ -19,4 +21,38 @@ func init() {
 		panic(err)
 	}
 	fmt.Println("✓ Templates chargés avec succès")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/index.html")
+}
+
+func gameHandler(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		Board         [6][7]int
+		CurrentPlayer int
+		GameOver      bool
+		Message       string
+	}{
+		Board:         board,
+		CurrentPlayer: currentPlayer,
+		GameOver:      gameOver,
+	}
+
+	if gameOver {
+		if winner := checkWin(); winner != 0 {
+			data.Message = "Joueur " + strconv.Itoa(winner) + " gagne ! 🎉"
+		} else {
+			data.Message = "Match nul ! 😅"
+		}
+	} else {
+		color := map[int]string{1: "(Rouge)", 2: "(Jaune)"}[currentPlayer]
+		data.Message = "Tour du Joueur " + strconv.Itoa(currentPlayer) + " " + color
+	}
+
+	err := tpl.ExecuteTemplate(w, "jeu.html", data)
+	if err != nil {
+		fmt.Println("ERREUR Template:", err)
+		http.Error(w, "Erreur template", http.StatusInternalServerError)
+	}
 }
