dd0f394ddc088b380fa59ca809cc8705f9185994
Wesleyyy09 <231713634+Wesleyyy09@users.noreply.github.com>
2025-10-14 13:53:19 +0200

    Ajout du Plateau

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
-// Gabriel à faire
+package main
+
+import (
+	"fmt"
+	"html/template"
+)
+
+// Plateau 6x7 (0=vide, 1=rouge, 2=jaune)
+var board [6][7]int
+var currentPlayer = 1
+var gameOver = false
+
+var tpl *template.Template
+
+func init() {
+	var err error
+	tpl, err = template.ParseGlob("templates/*.html")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("✓ Templates chargés avec succès")
+}
